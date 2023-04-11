b07ca5a4eee6607a27be0121ddcd55fd7e9ef469
Boris Zalov <boriszalovs1@gmail.com>
2023-04-12 00:52:51 +0300

    icon fix

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,7 +7,7 @@ const user = useSupabaseUser()
         <nav class="navbar bg-body-tertiary navbar-expand-lg fixed-top shadow">
             <div class="container-fluid">
                 <NuxtLink to="/" class="navbar-brand">
-                    <nuxt-img src="/icon.png" format="webp" alt="Logo" class="d-inline-block align-text-top mx-1" />
+                    <img src="/favicon.ico" alt="Logo" class="d-inline-block align-text-top mx-1" />
                     Richat
                 </NuxtLink>
 
